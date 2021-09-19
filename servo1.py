e6d295a0edbc59bdd2f480ac7297d9953b64dac7
Jordyn Brosemer <brosemerj@Jordyns-MacBook-Pro.local>
2021-09-18 21:07:12 -0400

    third commit

--- servo1.py
+++ servo1.py
@@ -1,8 +1,18 @@
 import time
+import drivers
 from adafruit_servokit import ServoKit
+from datetime import datetime
 
 kit = ServoKit(channels=16)
-kit.servo[0].angle = 180
-time.sleep(1)
-kit.servo[0].angle = 0
-time.sleep(1)
+display = drivers.Lcd()
+try:
+    while True:
+        display.lcd_display_string("Not enough time",1)
+        display.lcd_display_string(datetime.now().strftime("%M"),2)
+        kit.servo[0].angle = 180
+        time.sleep(1)
+        kit.servo[0].angle = 0
+        time.sleep(1)
+except KeyboardInterrupt:
+    print("Exit")
+    
