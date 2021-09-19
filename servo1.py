e9b0ba77a47382d9611955403227c8d06f6e51e7
Jordyn Brosemer <brosemerj@Jordyns-MacBook-Pro.local>
2021-09-18 21:34:50 -0400

    comments

--- servo1.py
+++ servo1.py
@@ -1,17 +1,23 @@
 import time
+#importing lcd drivers to drive the LCD board found from The_Raspberry_Pi_Guy Github
 import drivers
+#importing servokit to drive multiple servos using servodriver
 from adafruit_servokit import ServoKit
+#import time of day library
 from datetime import datetime
-
+#set number of servo channels
 kit = ServoKit(channels=16)
+#define the lcd drivers to be used
 display = drivers.Lcd()
 try:
     while True:
-        minute = int(datetime.now().strftime("%S"))
-        print(minute)
+        #get the current #of seconds in the minute and set it as an integer
+        seconds = int(datetime.now().strftime("%S"))
+        #print using lcd_display_string my apparent lack of time
         display.lcd_display_string("Not enough time",1)
-        display.lcd_display_string(datetime.now().strftime("%S"),2)
-        kit.servo[0].angle = minute*3
+        #print current seconds
+        display.lcd_display_string(seconds,2)
+        kit.servo[0].angle = seconds*3
 except KeyboardInterrupt:
     kit.servo[0].angle = 0
     print("Exit")
