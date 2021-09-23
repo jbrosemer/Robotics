1ae1b40a2b501692020218f438ae1ba96d6f136f
Jordyn Brosemer <brosemerj@Jordyns-MacBook-Pro.local>
2021-09-23 19:18:38 -0400

    final changes + comments

--- servo1.py
+++ servo1.py
@@ -26,16 +26,16 @@ multiplier = 1
 try:
     while True:
         if current:
-            #get all the times and set them as ints
-            #AKA we are not speeding up time
+            # get all the times and set them as ints
+            # AKA we are not speeding up time
             seconds = int(datetime.now().strftime("%S"))
             tensminutes = minuter(int(datetime.now().strftime("%M")))
             onesminutes = int(datetime.now().strftime("%M"))%10
             hours = int(datetime.now().strftime("%H"))
-            #unmilitarize the time
+            # unmilitarize the time
             if hours > 12:
                 hours-=12
-            #check if speed up button is pressed
+            # check if speed up button is pressed
             if button1.is_pressed:
                 debouncer+=1
                 if debouncer > 5:
@@ -44,23 +44,26 @@ try:
                     debouncer = 0
             else:
                 debouncer  = 0
-                #constantly check if normalize button is being pressed while in sped up time
+                # constantly check if normalize button is being pressed while in sped up time
         elif button2.is_pressed:
-            #go back to current time and reset multiplier
+            # go back to current time and reset multiplier
             current = True
             multiplier = 1
             debouncer = 0
+        # if we are in time multiplying mode
         elif multiplier > 1:
-            #check if multiplier is greater than 1
+            # check to speed time up faster
             if button1.is_pressed:
                 debouncer += 1
                 if debouncer > 5:
                     multiplier = multiplier * 2
                     debouncer = 0
+            # reset time needed to press the button if released
             else:
                 debouncer = 0
+            # scale the seconds with the multiplier
             seconds = seconds + multiplier
-            print(multiplier)
+            # basically just how a clock works 60 seconds add a 1 minute and reset seconds
             if seconds > 59:
                 seconds = 0
                 if multiplier < 100:
@@ -76,33 +79,39 @@ try:
                         if hours > 12:
                             hours -= 12
 
-
-        #print using lcd_display_string my apparent lack of time
+        # all display code is useless but the display was very helpful in debugging so i left it in
+        # print using lcd_display_string my apparent lack of time
         display.lcd_display_string("What time is it?",1)
-        #print current seconds
+        # print current seconds to LCD
         if seconds < 9:
             display.lcd_display_string(str(hours) + ":" + str(tensminutes) + str(onesminutes) + ":0" + str(seconds),2)
         else:
             display.lcd_display_string(str(hours) + ":" + str(tensminutes) +str(onesminutes) + ":" + str(seconds),2)
+        # seconds LED active when odd and when time is sped up
         if multiplier > 1:
             led2.on()
         if seconds%2 == 1:
             led2.on()
         else:
             led2.off()
+        # hours on when 10,11 or, 12 o clock
         if hours > 9:
             led1.on()
         else:
             led1.off()
+        # set ones minute servo to 20 degree turn per minute passing because 9 holes to cover
         kit.servo[3].angle = 180 - ((onesminutes) * 20)
-        #kit.servo[2].angle = seconds*3
+        # kit.servo[2].angle = seconds*3
+        # set tens minute servo to 36 degree turn per minute passing because only 5 holes to cover
         kit.servo[1].angle = tensminutes*36
+        # set hours servo 20 degree turn per minute passing because 9 holes to cover
         kit.servo[0].angle = 180 - ((hours%10) * 20)
 
-#exit the loop on pressing ctrl+c
+# exit the loop on pressing ctrl+c
 except KeyboardInterrupt:
+    # clear the LCD display
     display.lcd_clear()
-    #reset Servo angle
+    # reset Servo angles to their respective 'zeros'
     kit.servo[0].angle = 180
     kit.servo[1].angle = 0
     kit.servo[3].angle = 180
